6c059bb648e3ee4d22cafda9e0773f525f40d7b3
ABDUL RASHID OMENI <75305745+aromeni@users.noreply.github.com>
2023-12-11 07:23:44 +0000

    Re-instate card item to previous

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -2,26 +2,25 @@
 
 const ItemCard = ({ item, onDelete }) => {
   return (
-    <div className="bg-gray-100 p-4 rounded-md justify-center">
-      <img src={item.image} alt="item" />
-      <div>
-        <h5 data-field="id">Item ID: {item.id}</h5>
-        <p>User ID: {item.user_id}</p>
-        <div>
-          <p>Lat: {item.lat}</p>
-          <p>Lon: {item.lon}</p>
-          <p>Keywords: {item.keywords}</p>
-          <p>Description: {item.description}</p>
-        </div>
-        <button
-          data-action="delete"
-          className="bg-gray-600 w-full mt-2  text-white mx-auto px-4 py-2 hover:bg-red-600 rounded shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-300 ease-in-out"
-          onClick={() => onDelete(item.id)}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
+    <div key={item.id} className="bg-gray-100 p-4 rounded-md justify-center">
+                <div>
+                    <img src={item.image} alt="item" />
+                    <h5 data-field="id">Item ID: {item.id}</h5>
+                    <p data-field="user_id">User ID: {item.user_id}</p>
+                    <p data-field="lat">Lat: {item.lat}</p>
+                    <p data-field="lon">Lon: {item.lon}</p>
+                    <p data-field="description">Description: {item.description}</p>
+                </div>
+                <div>
+                    <button
+                        data-action="delete"
+                        className="bg-gray-600 w-full mt-2  text-white mx-auto px-4 py-2 hover:bg-red-600 rounded shadow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition duration-300 ease-in-out"
+                        onClick={() => onDelete(item.id)}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
   );
 };
 
